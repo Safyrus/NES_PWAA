9437e340331b7995eb838d9a1dd4bd6c7f47c23f
Safyrus <54881649+Safyrus@users.noreply.github.com>
2022-12-27 01:47:27 +0100

    add script to fusion images

---
+++ py/imgEncoder/fusion_bkg_chr.py
@@ -0,0 +1,42 @@
+import sys
+import glob
+from PIL import Image
+
+if len(sys.argv) <= 3:
+    print(sys.argv[0], "<background>", "<char folder>", "<output folder>", "[align]", "[x offset]", "[y offset]")
+    exit(0)
+
+# args
+background_img = sys.argv[1]
+character_folder = sys.argv[2]
+output_folder = sys.argv[3]
+align = 0  # 0=center, 1=left, 2=right
+offset_x = 0
+offset_y = 0
+if len(sys.argv) > 4:
+    align = int(sys.argv[4])
+if len(sys.argv) > 5:
+    offset_x = int(sys.argv[5])
+if len(sys.argv) > 6:
+    offset_y = int(sys.argv[6])
+
+# find all character images
+files = glob.glob(character_folder + '/**/*.png', recursive=True)
+
+background = Image.open(background_img)
+
+i = 0
+w, h = background.size
+for fn in files:
+    char = Image.open(fn)
+    tmp = background.copy()
+    if align == 1:
+        x = offset_x
+    elif align == 2:
+        x = w - char.size[0] - offset_x
+    else:
+        x = (w - char.size[0])//2
+    y = h - char.size[1] - offset_y
+    tmp.paste(char, (x, y), char)
+    tmp.save(output_folder + str(i) + ".png")
+    i += 1
