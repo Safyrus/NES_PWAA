88c3e3cc1382fda64b412360e4612c5717f1df86
Safyrus <54881649+Safyrus@users.noreply.github.com>
2023-03-08 18:45:54 +0100

    update data

--- asm/data/imgs.asm
+++ asm/data/imgs.asm
@@ -13,6 +13,8 @@ img_4:
 .incbin "imgs/img_4.bin" ;../../data/sprite2.png;../../data/sprite3.png
 img_5:
 .incbin "imgs/img_5.bin" ;../../data/sprite3.png;../../data/sprite4.png
+img_6:
+.incbin "imgs/img_6.bin" ;../../data/sprite4.png;../../data/sprite1.png
 
 .segment "CODE_BNK"
 img_bkg_table:
@@ -26,7 +28,7 @@ img_anim_table:
 default:
 .byte $01 ; size
 img_anim_0:
-.byte $11 ; size
+.byte $15 ; size
 .byte $10 ; time
 .word (img_2 & $FFFF) ; ../../data/empty.png;../../data/sprite1.png
 .byte $c0 ; bank
@@ -39,13 +41,14 @@ img_anim_0:
 .byte $10 ; time
 .word (img_5 & $FFFF) ; ../../data/sprite3.png;../../data/sprite4.png
 .byte $c0 ; bank
+.byte $00 ; time
+.word (img_6 & $FFFF) ; ../../data/sprite4.png;../../data/sprite1.png
+.byte $c0 ; bank
 
 palette_table:
-.byte $0F, $0F, $0F, $0F ; [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]
-.byte $21, $11, $01, $0F ; [105, 158, 252], [30, 74, 157], [1, 26, 81], [0, 0, 0]
-.byte $21, $02, $0F, $12 ; [105, 158, 252], [15, 15, 101], [0, 0, 0], [56, 55, 188]
-.byte $21, $02, $01, $0F ; [105, 158, 252], [15, 15, 101], [1, 26, 81], [0, 0, 0]
-.byte $21, $15, $1A, $24 ; [105, 158, 252], [132, 35, 92], [26, 107, 5], [206, 109, 241]
-.byte $21, $02, $0F, $10 ; [105, 158, 252], [15, 15, 101], [0, 0, 0], [254, 255, 255]
-.byte $21, $12, $15, $24 ; [105, 158, 252], [56, 55, 188], [132, 35, 92], [206, 109, 241]
+.byte $0F, $0F, $0F, $0F ; BLACK[0, 0, 0], BLACK[0, 0, 0], BLACK[0, 0, 0], BLACK[0, 0, 0]
+.byte $0F, $01, $11, $21 ; BLACK[0, 0, 0], DARKEST BLUE[1, 26, 81], DARK BLUE[30, 74, 157], BLUE[105, 158, 252]
+.byte $0F, $39, $37, $11 ; BLACK[0, 0, 0], LIGHT OLIVE[201, 226, 158], LIGHT ORANGE[232, 208, 170], DARK BLUE[30, 74, 157]
+.byte $0F, $39, $11, $21 ; BLACK[0, 0, 0], LIGHT OLIVE[201, 226, 158], DARK BLUE[30, 74, 157], BLUE[105, 158, 252]
+.byte $0F, $26, $22, $30 ; BLACK[0, 0, 0], RED[222, 124, 112], SLATEBLUE[137, 135, 255], WHITE[254, 255, 255]
 
