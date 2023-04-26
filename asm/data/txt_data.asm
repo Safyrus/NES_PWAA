954d917b966fc1755222d2a30179c336bd18ecf0
Safyrus <54881649+Safyrus@users.noreply.github.com>
2023-04-26 19:24:59 +0200

    change text & img for nes pwaa data

--- asm/data/txt_data.asm
+++ asm/data/txt_data.asm
@@ -1,14 +1,32 @@
 ; TODO description
 
 .segment "TXT_BNK"
-.incbin "text.bin"
+.incbin "./text.bin"
 
 .segment "CODE_BNK"
 lz_bnk_table:
-.byte $88
+.byte $83
+.byte $83
+.byte $84
+.byte $84
+.byte $85
+.byte $85
+.byte $86
 
 lz_adr_table_lo:
 .byte $00
+.byte $55
+.byte $F5
+.byte $59
+.byte $2F
+.byte $8C
+.byte $6B
 
 lz_adr_table_hi:
 .byte $A0
+.byte $B3
+.byte $A4
+.byte $B5
+.byte $A5
+.byte $B6
+.byte $A8
