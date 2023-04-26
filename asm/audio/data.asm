4eef10be1dedcac8cc8b50fb3edea755e70adf75
Safyrus <54881649+Safyrus@users.noreply.github.com>
2023-04-26 19:23:24 +0200

    change audio for nes pwaa music & sfx

--- asm/audio/data.asm
+++ asm/audio/data.asm
@@ -1,12 +1,4 @@
-song_arpeggio = 0
-song_melody = 1
-song_max = 2
-
-sfx_bip_high = 0
-sfx_bip = 1
-sfx_bip_low = 2
-sfx_max = 3
 
 .segment "MUS_BNK"
-.include "music.s"
 .include "sfx.s"
+.include "music.s"
