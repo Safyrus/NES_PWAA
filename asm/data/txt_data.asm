b0caa710bd06b684c62579fc9fe8010e9c4c3e3e
Safyrus <54881649+Safyrus@users.noreply.github.com>
2023-05-04 12:18:15 +0200

    add name tiles & update data

--- asm/data/txt_data.asm
+++ asm/data/txt_data.asm
@@ -15,18 +15,18 @@ lz_bnk_table:
 
 lz_adr_table_lo:
 .byte $00
-.byte $55
-.byte $F5
-.byte $59
-.byte $2F
-.byte $8C
 .byte $6B
+.byte $29
+.byte $AA
+.byte $AA
+.byte $2F
+.byte $22
 
 lz_adr_table_hi:
 .byte $A0
 .byte $B3
-.byte $A4
+.byte $A5
 .byte $B5
 .byte $A5
-.byte $B6
-.byte $A8
+.byte $B7
+.byte $A9
