62caa19795c3173b5019633ff9b7780d82bc2168
Safyrus <54881649+Safyrus@users.noreply.github.com>
2024-07-24 16:39:41 +0200

    add robot images and animations

--- asm/data/imgs.asm
+++ asm/data/imgs.asm
@@ -2,35 +2,95 @@
 
 .segment "IMGS_BNK"
 img_0:
-.incbin "imgs/img_0.bin" ;../../data/anim\..\empty.png
+.incbin "imgs/img_0.bin" ;..\..\data\empty.png
 img_1:
-.incbin "imgs/img_1.bin" ;../../data/anim\..\img\star_sky_castle.png
+.incbin "imgs/img_1.bin" ;..\..\data\img\star_sky_castle.png
 img_2:
-.incbin "imgs/img_2.bin" ;../../data/anim\..\img\green_landscape.png
+.incbin "imgs/img_2.bin" ;..\..\data\img\green_landscape.png
 img_3:
-.incbin "imgs/img_3.bin" ;../../data/anim\..\img\orange_dune.png
+.incbin "imgs/img_3.bin" ;..\..\data\img\orange_dune.png
 img_4:
-.incbin "imgs/img_4.bin" ;../../data/anim\..\img\scene_1.png
+.incbin "imgs/img_4.bin" ;..\..\data\img\scene_1.png
 img_5:
-.incbin "imgs/img_5.bin" ;../../data/anim\..\img\scene_1.png;../../data/anim\..\img\robot\teach\idle_1.png
+.incbin "imgs/img_5.bin" ;..\..\data\img\scene_1.png;..\..\data\img\robot\stand\smile1.png
 img_6:
-.incbin "imgs/img_6.bin" ;../../data/anim\..\img\robot\teach\idle_1.png;../../data/anim\..\img\robot\teach\idle_2.png
+.incbin "imgs/img_6.bin" ;..\..\data\img\robot\stand\smile1.png;..\..\data\img\robot\stand\smile2.png
 img_7:
-.incbin "imgs/img_7.bin" ;../../data/anim\..\img\robot\teach\idle_2.png;../../data/anim\..\img\robot\teach\idle_1.png
+.incbin "imgs/img_7.bin" ;..\..\data\img\robot\stand\smile2.png;..\..\data\img\robot\stand\smile1.png
 img_8:
-.incbin "imgs/img_8.bin" ;../../data/anim\..\img\scene_1.png;../../data/anim\..\img\robot\teach\talk_1.png
+.incbin "imgs/img_8.bin" ;..\..\data\img\robot\stand\smile1.png;..\..\data\img\robot\stand\smile_talk.png
 img_9:
-.incbin "imgs/img_9.bin" ;../../data/anim\..\img\robot\teach\talk_1.png;../../data/anim\..\img\robot\teach\talk_2.png
+.incbin "imgs/img_9.bin" ;..\..\data\img\robot\stand\smile_talk.png;..\..\data\img\robot\stand\smile1.png
 img_10:
-.incbin "imgs/img_10.bin" ;../../data/anim\..\img\robot\teach\talk_2.png;../../data/anim\..\img\robot\teach\talk_1.png
+.incbin "imgs/img_10.bin" ;..\..\data\img\scene_1.png;..\..\data\img\robot\stand\neutral1.png
+img_11:
+.incbin "imgs/img_11.bin" ;..\..\data\img\robot\stand\neutral1.png;..\..\data\img\robot\stand\neutral2.png
+img_12:
+.incbin "imgs/img_12.bin" ;..\..\data\img\robot\stand\neutral2.png;..\..\data\img\robot\stand\neutral1.png
+img_13:
+.incbin "imgs/img_13.bin" ;..\..\data\img\robot\stand\neutral1.png;..\..\data\img\robot\stand\neutral_talk.png
+img_14:
+.incbin "imgs/img_14.bin" ;..\..\data\img\robot\stand\neutral_talk.png;..\..\data\img\robot\stand\neutral1.png
+img_15:
+.incbin "imgs/img_15.bin" ;..\..\data\img\scene_1.png;..\..\data\img\robot\stand\sad1.png
+img_16:
+.incbin "imgs/img_16.bin" ;..\..\data\img\robot\stand\sad1.png;..\..\data\img\robot\stand\sad2.png
+img_17:
+.incbin "imgs/img_17.bin" ;..\..\data\img\robot\stand\sad2.png;..\..\data\img\robot\stand\sad1.png
+img_18:
+.incbin "imgs/img_18.bin" ;..\..\data\img\robot\stand\sad1.png;..\..\data\img\robot\stand\sad_talk.png
+img_19:
+.incbin "imgs/img_19.bin" ;..\..\data\img\robot\stand\sad_talk.png;..\..\data\img\robot\stand\sad1.png
+img_20:
+.incbin "imgs/img_20.bin" ;..\..\data\img\scene_1.png;..\..\data\img\robot\stand\sigh1.png
+img_21:
+.incbin "imgs/img_21.bin" ;..\..\data\img\robot\stand\sigh1.png;..\..\data\img\robot\stand\sigh2.png
+img_22:
+.incbin "imgs/img_22.bin" ;..\..\data\img\robot\stand\sigh2.png;..\..\data\img\robot\stand\sigh1.png
+img_23:
+.incbin "imgs/img_23.bin" ;..\..\data\img\robot\stand\sigh1.png;..\..\data\img\robot\stand\sigh_talk.png
+img_24:
+.incbin "imgs/img_24.bin" ;..\..\data\img\robot\stand\sigh_talk.png;..\..\data\img\robot\stand\sigh1.png
+img_25:
+.incbin "imgs/img_25.bin" ;..\..\data\img\scene_1.png;..\..\data\img\robot\boot\robot_boot1.png
+img_26:
+.incbin "imgs/img_26.bin" ;..\..\data\img\robot\boot\robot_boot1.png;..\..\data\img\robot\boot\robot_boot2.png
+img_27:
+.incbin "imgs/img_27.bin" ;..\..\data\img\robot\boot\robot_boot2.png;..\..\data\img\robot\boot\robot_boot3.png
+img_28:
+.incbin "imgs/img_28.bin" ;..\..\data\img\robot\boot\robot_boot3.png;..\..\data\img\robot\boot\robot_boot4.png
+img_29:
+.incbin "imgs/img_29.bin" ;..\..\data\img\robot\boot\robot_boot4.png;..\..\data\img\robot\boot\robot_boot5.png
+img_30:
+.incbin "imgs/img_30.bin" ;..\..\data\img\robot\boot\robot_boot5.png;..\..\data\img\robot\boot\robot_boot6.png
+img_31:
+.incbin "imgs/img_31.bin" ;..\..\data\img\robot\boot\robot_boot6.png;..\..\data\img\robot\boot\robot_boot7.png
+img_32:
+.incbin "imgs/img_32.bin" ;..\..\data\img\robot\boot\robot_boot7.png;..\..\data\img\robot\boot\robot_boot8.png
+img_33:
+.incbin "imgs/img_33.bin" ;..\..\data\img\robot\boot\robot_boot8.png;..\..\data\img\robot\boot\robot_boot9.png
+img_34:
+.incbin "imgs/img_34.bin" ;..\..\data\img\robot\boot\robot_boot9.png;..\..\data\img\robot\boot\robot_boot10.png
+img_35:
+.incbin "imgs/img_35.bin" ;..\..\data\img\robot\boot\robot_boot10.png;..\..\data\img\robot\boot\robot_boot11.png
+img_36:
+.incbin "imgs/img_36.bin" ;..\..\data\img\robot\boot\robot_boot11.png;..\..\data\img\robot\boot\robot_boot12.png
+img_37:
+.incbin "imgs/img_37.bin" ;..\..\data\img\robot\boot\robot_boot12.png;..\..\data\img\robot\boot\robot_boot13.png
+img_38:
+.incbin "imgs/img_38.bin" ;..\..\data\img\robot\boot\robot_boot13.png;..\..\data\img\robot\boot\robot_boot14.png
+img_39:
+.incbin "imgs/img_39.bin" ;..\..\data\img\robot\boot\robot_boot14.png;..\..\data\img\robot\boot\robot_boot1.png
+img_40:
+.incbin "imgs/img_40.bin" ;..\..\data\img\robot\boot\robot_boot1.png;..\..\data\img\robot\boot\robot_boot1.png
 
 .segment "ANIM_BNK"
 img_bkg_table:
-.word (img_0 & $1FFF) + SEGMENT_IMGS_START_ADR ; ../../data/anim\..\empty.png
-.word (img_1 & $1FFF) + SEGMENT_IMGS_START_ADR ; ../../data/anim\..\img\star_sky_castle.png
-.word (img_2 & $1FFF) + SEGMENT_IMGS_START_ADR ; ../../data/anim\..\img\green_landscape.png
-.word (img_3 & $1FFF) + SEGMENT_IMGS_START_ADR ; ../../data/anim\..\img\orange_dune.png
-.word (img_4 & $1FFF) + SEGMENT_IMGS_START_ADR ; ../../data/anim\..\img\scene_1.png
+.word (img_0 & $1FFF) + SEGMENT_IMGS_START_ADR ; ..\..\data\empty.png
+.word (img_1 & $1FFF) + SEGMENT_IMGS_START_ADR ; ..\..\data\img\star_sky_castle.png
+.word (img_2 & $1FFF) + SEGMENT_IMGS_START_ADR ; ..\..\data\img\green_landscape.png
+.word (img_3 & $1FFF) + SEGMENT_IMGS_START_ADR ; ..\..\data\img\orange_dune.png
+.word (img_4 & $1FFF) + SEGMENT_IMGS_START_ADR ; ..\..\data\img\scene_1.png
 img_bkg_table_bank:
 .byte IMG_BNK + $0
 .byte IMG_BNK + $0
@@ -43,24 +103,145 @@ img_anim_table:
 .byte $01 ; size
 img_anim_1:
 .byte $0D ; size
-.byte $1E ; time
-.word (img_5 & $1FFF) + SEGMENT_IMGS_START_ADR ; ../../data/anim\..\img\scene_1.png;../../data/anim\..\img\robot\teach\idle_1.png
+.byte $0F ; time
+.word (img_5 & $1FFF) + SEGMENT_IMGS_START_ADR ; ..\..\data\img\scene_1.png;..\..\data\img\robot\stand\smile1.png
 .byte IMG_BNK + $0 ; bank
-.byte $1E ; time
-.word (img_6 & $1FFF) + SEGMENT_IMGS_START_ADR ; ../../data/anim\..\img\robot\teach\idle_1.png;../../data/anim\..\img\robot\teach\idle_2.png
+.byte $0F ; time
+.word (img_6 & $1FFF) + SEGMENT_IMGS_START_ADR ; ..\..\data\img\robot\stand\smile1.png;..\..\data\img\robot\stand\smile2.png
 .byte IMG_BNK + $0 ; bank
 .byte $00 ; time
-.word (img_7 & $1FFF) + SEGMENT_IMGS_START_ADR ; ../../data/anim\..\img\robot\teach\idle_2.png;../../data/anim\..\img\robot\teach\idle_1.png
+.word (img_7 & $1FFF) + SEGMENT_IMGS_START_ADR ; ..\..\data\img\robot\stand\smile2.png;..\..\data\img\robot\stand\smile1.png
 .byte IMG_BNK + $0 ; bank
 img_anim_2:
 .byte $0D ; size
-.byte $1E ; time
-.word (img_8 & $1FFF) + SEGMENT_IMGS_START_ADR ; ../../data/anim\..\img\scene_1.png;../../data/anim\..\img\robot\teach\talk_1.png
+.byte $0F ; time
+.word (img_5 & $1FFF) + SEGMENT_IMGS_START_ADR ; ..\..\data\img\scene_1.png;..\..\data\img\robot\stand\smile1.png
 .byte IMG_BNK + $0 ; bank
-.byte $1E ; time
-.word (img_9 & $1FFF) + SEGMENT_IMGS_START_ADR ; ../../data/anim\..\img\robot\teach\talk_1.png;../../data/anim\..\img\robot\teach\talk_2.png
+.byte $0F ; time
+.word (img_8 & $1FFF) + SEGMENT_IMGS_START_ADR ; ..\..\data\img\robot\stand\smile1.png;..\..\data\img\robot\stand\smile_talk.png
 .byte IMG_BNK + $0 ; bank
 .byte $00 ; time
-.word (img_10 & $1FFF) + SEGMENT_IMGS_START_ADR ; ../../data/anim\..\img\robot\teach\talk_2.png;../../data/anim\..\img\robot\teach\talk_1.png
+.word (img_9 & $1FFF) + SEGMENT_IMGS_START_ADR ; ..\..\data\img\robot\stand\smile_talk.png;..\..\data\img\robot\stand\smile1.png
+.byte IMG_BNK + $0 ; bank
+img_anim_3:
+.byte $0D ; size
+.byte $0F ; time
+.word (img_10 & $1FFF) + SEGMENT_IMGS_START_ADR ; ..\..\data\img\scene_1.png;..\..\data\img\robot\stand\neutral1.png
+.byte IMG_BNK + $0 ; bank
+.byte $0F ; time
+.word (img_11 & $1FFF) + SEGMENT_IMGS_START_ADR ; ..\..\data\img\robot\stand\neutral1.png;..\..\data\img\robot\stand\neutral2.png
+.byte IMG_BNK + $0 ; bank
+.byte $00 ; time
+.word (img_12 & $1FFF) + SEGMENT_IMGS_START_ADR ; ..\..\data\img\robot\stand\neutral2.png;..\..\data\img\robot\stand\neutral1.png
+.byte IMG_BNK + $0 ; bank
+img_anim_4:
+.byte $0D ; size
+.byte $0F ; time
+.word (img_10 & $1FFF) + SEGMENT_IMGS_START_ADR ; ..\..\data\img\scene_1.png;..\..\data\img\robot\stand\neutral1.png
+.byte IMG_BNK + $0 ; bank
+.byte $0F ; time
+.word (img_13 & $1FFF) + SEGMENT_IMGS_START_ADR ; ..\..\data\img\robot\stand\neutral1.png;..\..\data\img\robot\stand\neutral_talk.png
+.byte IMG_BNK + $0 ; bank
+.byte $00 ; time
+.word (img_14 & $1FFF) + SEGMENT_IMGS_START_ADR ; ..\..\data\img\robot\stand\neutral_talk.png;..\..\data\img\robot\stand\neutral1.png
+.byte IMG_BNK + $0 ; bank
+img_anim_5:
+.byte $0D ; size
+.byte $0F ; time
+.word (img_15 & $1FFF) + SEGMENT_IMGS_START_ADR ; ..\..\data\img\scene_1.png;..\..\data\img\robot\stand\sad1.png
+.byte IMG_BNK + $0 ; bank
+.byte $0F ; time
+.word (img_16 & $1FFF) + SEGMENT_IMGS_START_ADR ; ..\..\data\img\robot\stand\sad1.png;..\..\data\img\robot\stand\sad2.png
+.byte IMG_BNK + $0 ; bank
+.byte $00 ; time
+.word (img_17 & $1FFF) + SEGMENT_IMGS_START_ADR ; ..\..\data\img\robot\stand\sad2.png;..\..\data\img\robot\stand\sad1.png
+.byte IMG_BNK + $0 ; bank
+img_anim_6:
+.byte $0D ; size
+.byte $0F ; time
+.word (img_15 & $1FFF) + SEGMENT_IMGS_START_ADR ; ..\..\data\img\scene_1.png;..\..\data\img\robot\stand\sad1.png
+.byte IMG_BNK + $0 ; bank
+.byte $0F ; time
+.word (img_18 & $1FFF) + SEGMENT_IMGS_START_ADR ; ..\..\data\img\robot\stand\sad1.png;..\..\data\img\robot\stand\sad_talk.png
+.byte IMG_BNK + $0 ; bank
+.byte $00 ; time
+.word (img_19 & $1FFF) + SEGMENT_IMGS_START_ADR ; ..\..\data\img\robot\stand\sad_talk.png;..\..\data\img\robot\stand\sad1.png
+.byte IMG_BNK + $0 ; bank
+img_anim_7:
+.byte $0D ; size
+.byte $0F ; time
+.word (img_20 & $1FFF) + SEGMENT_IMGS_START_ADR ; ..\..\data\img\scene_1.png;..\..\data\img\robot\stand\sigh1.png
+.byte IMG_BNK + $0 ; bank
+.byte $0F ; time
+.word (img_21 & $1FFF) + SEGMENT_IMGS_START_ADR ; ..\..\data\img\robot\stand\sigh1.png;..\..\data\img\robot\stand\sigh2.png
+.byte IMG_BNK + $0 ; bank
+.byte $00 ; time
+.word (img_22 & $1FFF) + SEGMENT_IMGS_START_ADR ; ..\..\data\img\robot\stand\sigh2.png;..\..\data\img\robot\stand\sigh1.png
+.byte IMG_BNK + $0 ; bank
+img_anim_8:
+.byte $0D ; size
+.byte $0F ; time
+.word (img_20 & $1FFF) + SEGMENT_IMGS_START_ADR ; ..\..\data\img\scene_1.png;..\..\data\img\robot\stand\sigh1.png
+.byte IMG_BNK + $0 ; bank
+.byte $0F ; time
+.word (img_23 & $1FFF) + SEGMENT_IMGS_START_ADR ; ..\..\data\img\robot\stand\sigh1.png;..\..\data\img\robot\stand\sigh_talk.png
+.byte IMG_BNK + $0 ; bank
+.byte $00 ; time
+.word (img_24 & $1FFF) + SEGMENT_IMGS_START_ADR ; ..\..\data\img\robot\stand\sigh_talk.png;..\..\data\img\robot\stand\sigh1.png
+.byte IMG_BNK + $0 ; bank
+img_anim_9:
+.byte $3D ; size
+.byte $0F ; time
+.word (img_25 & $1FFF) + SEGMENT_IMGS_START_ADR ; ..\..\data\img\scene_1.png;..\..\data\img\robot\boot\robot_boot1.png
+.byte IMG_BNK + $0 ; bank
+.byte $0F ; time
+.word (img_26 & $1FFF) + SEGMENT_IMGS_START_ADR ; ..\..\data\img\robot\boot\robot_boot1.png;..\..\data\img\robot\boot\robot_boot2.png
+.byte IMG_BNK + $0 ; bank
+.byte $0F ; time
+.word (img_27 & $1FFF) + SEGMENT_IMGS_START_ADR ; ..\..\data\img\robot\boot\robot_boot2.png;..\..\data\img\robot\boot\robot_boot3.png
+.byte IMG_BNK + $0 ; bank
+.byte $0F ; time
+.word (img_28 & $1FFF) + SEGMENT_IMGS_START_ADR ; ..\..\data\img\robot\boot\robot_boot3.png;..\..\data\img\robot\boot\robot_boot4.png
+.byte IMG_BNK + $0 ; bank
+.byte $0F ; time
+.word (img_29 & $1FFF) + SEGMENT_IMGS_START_ADR ; ..\..\data\img\robot\boot\robot_boot4.png;..\..\data\img\robot\boot\robot_boot5.png
+.byte IMG_BNK + $0 ; bank
+.byte $0F ; time
+.word (img_30 & $1FFF) + SEGMENT_IMGS_START_ADR ; ..\..\data\img\robot\boot\robot_boot5.png;..\..\data\img\robot\boot\robot_boot6.png
+.byte IMG_BNK + $0 ; bank
+.byte $0F ; time
+.word (img_31 & $1FFF) + SEGMENT_IMGS_START_ADR ; ..\..\data\img\robot\boot\robot_boot6.png;..\..\data\img\robot\boot\robot_boot7.png
+.byte IMG_BNK + $0 ; bank
+.byte $0F ; time
+.word (img_32 & $1FFF) + SEGMENT_IMGS_START_ADR ; ..\..\data\img\robot\boot\robot_boot7.png;..\..\data\img\robot\boot\robot_boot8.png
+.byte IMG_BNK + $0 ; bank
+.byte $0F ; time
+.word (img_33 & $1FFF) + SEGMENT_IMGS_START_ADR ; ..\..\data\img\robot\boot\robot_boot8.png;..\..\data\img\robot\boot\robot_boot9.png
+.byte IMG_BNK + $0 ; bank
+.byte $0F ; time
+.word (img_34 & $1FFF) + SEGMENT_IMGS_START_ADR ; ..\..\data\img\robot\boot\robot_boot9.png;..\..\data\img\robot\boot\robot_boot10.png
+.byte IMG_BNK + $0 ; bank
+.byte $0F ; time
+.word (img_35 & $1FFF) + SEGMENT_IMGS_START_ADR ; ..\..\data\img\robot\boot\robot_boot10.png;..\..\data\img\robot\boot\robot_boot11.png
+.byte IMG_BNK + $0 ; bank
+.byte $0F ; time
+.word (img_36 & $1FFF) + SEGMENT_IMGS_START_ADR ; ..\..\data\img\robot\boot\robot_boot11.png;..\..\data\img\robot\boot\robot_boot12.png
+.byte IMG_BNK + $0 ; bank
+.byte $0F ; time
+.word (img_37 & $1FFF) + SEGMENT_IMGS_START_ADR ; ..\..\data\img\robot\boot\robot_boot12.png;..\..\data\img\robot\boot\robot_boot13.png
+.byte IMG_BNK + $0 ; bank
+.byte $7F ; time
+.word (img_38 & $1FFF) + SEGMENT_IMGS_START_ADR ; ..\..\data\img\robot\boot\robot_boot13.png;..\..\data\img\robot\boot\robot_boot14.png
+.byte IMG_BNK + $0 ; bank
+.byte $00 ; time
+.word (img_39 & $1FFF) + SEGMENT_IMGS_START_ADR ; ..\..\data\img\robot\boot\robot_boot14.png;..\..\data\img\robot\boot\robot_boot1.png
+.byte IMG_BNK + $0 ; bank
+img_anim_10:
+.byte $09 ; size
+.byte $7F ; time
+.word (img_25 & $1FFF) + SEGMENT_IMGS_START_ADR ; ..\..\data\img\scene_1.png;..\..\data\img\robot\boot\robot_boot1.png
+.byte IMG_BNK + $0 ; bank
+.byte $00 ; time
+.word (img_40 & $1FFF) + SEGMENT_IMGS_START_ADR ; ..\..\data\img\robot\boot\robot_boot1.png;..\..\data\img\robot\boot\robot_boot1.png
 .byte IMG_BNK + $0 ; bank
 
