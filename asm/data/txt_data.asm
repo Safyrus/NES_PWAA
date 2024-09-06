2e16411661cf1e52a715b3a656eb7f7a6de4814d
Safyrus <54881649+Safyrus@users.noreply.github.com>
2024-09-06 17:46:48 +0200

    update asm data and git workflow

--- asm/data/txt_data.asm
+++ asm/data/txt_data.asm
@@ -7,11 +7,17 @@
 lz_bnk_table:
 .byte TXT_BNK + $00
 .byte TXT_BNK + $00
+.byte TXT_BNK + $01
+.byte TXT_BNK + $01
 
 lz_adr_table_lo:
 .byte $00
 .byte $68
+.byte $37
+.byte $4E
 
 lz_adr_table_hi:
 .byte $A0
 .byte $B0
+.byte $A0
+.byte $AF
