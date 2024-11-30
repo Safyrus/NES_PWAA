c6c8882c1d69a9587e490fc2269d4517a27c887b
Safyrus <54881649+Safyrus@users.noreply.github.com>
2024-11-30 14:30:51 +0100

    update data with better encoded images

--- asm/data/img/img_ptr.asm
+++ asm/data/img/img_ptr.asm
@@ -5,13 +5,13 @@
 
 img_ptr_list_lo:
 .byte (0 >> 0) & $FF
-.byte (109859 >> 0) & $FF
+.byte (107273 >> 0) & $FF
 
 img_ptr_list_hi:
 .byte ((0 >> 8) & $1F) + $A0
-.byte ((109859 >> 8) & $1F) + $A0
+.byte ((107273 >> 8) & $1F) + $A0
 
 img_ptr_list_bnk:
 .byte ((0 >> 13) & $7F) + IMG_BNK
-.byte ((109859 >> 13) & $7F) + IMG_BNK
+.byte ((107273 >> 13) & $7F) + IMG_BNK
 
