143e6157deb0c66656774e8c5c696cde0435595c
Safyrus <54881649+Safyrus@users.noreply.github.com>
2024-12-05 13:52:13 +0100

    update data and nes files

--- asm/data/img/img_ptr.asm
+++ asm/data/img/img_ptr.asm
@@ -5,13 +5,13 @@
 
 img_ptr_list_lo:
 .byte (0 >> 0) & $FF
-.byte (116629 >> 0) & $FF
+.byte (124696 >> 0) & $FF
 
 img_ptr_list_hi:
 .byte ((0 >> 8) & $1F) + $A0
-.byte ((116629 >> 8) & $1F) + $A0
+.byte ((124696 >> 8) & $1F) + $A0
 
 img_ptr_list_bnk:
 .byte ((0 >> 13) & $7F) + IMG_BNK
-.byte ((116629 >> 13) & $7F) + IMG_BNK
+.byte ((124696 >> 13) & $7F) + IMG_BNK
 
