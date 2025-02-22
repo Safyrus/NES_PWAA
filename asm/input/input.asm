df2707b6d879673e04ff1983ac77a439afc309cc
Safyrus <54881649+Safyrus@users.noreply.github.com>
2025-02-22 10:17:53 +0100

    remove old input code

--- asm/input/input.asm
+++
@@ -1,9 +0,0 @@
-    ; update player inputs
-    JSR update_input
-
-    .include "input_choice.asm"
-    .include "input_cr.asm"
-    .include "input_investigation.asm"
-    .include "input_normal.asm"
-
-    @input_end:
