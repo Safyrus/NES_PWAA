89adff43dca0a62812da9b02e2b8f57f59761d69
Safyrus <54881649+Safyrus@users.noreply.github.com>
2025-06-18 12:33:20 +0200

    change space char in japanese fonts

--- asm/data/img/img_ptr.asm
+++ asm/data/img/img_ptr.asm
@@ -1,20 +1,20 @@
 img_ptr_list_lo:
 .byte (0 >> 0) & $FF
-.byte (101653 >> 0) & $FF
+.byte (101687 >> 0) & $FF
 
 img_ptr_list_hi:
 .byte ((0 >> 8) & $1F) + $80
-.byte ((101653 >> 8) & $1F) + $80
+.byte ((101687 >> 8) & $1F) + $80
 
 img_ptr_list_bnk:
 .byte ((0 >> 13) & $7F) + IMG_BNK
-.byte ((101653 >> 13) & $7F) + IMG_BNK
+.byte ((101687 >> 13) & $7F) + IMG_BNK
 
 evi_ptr_list_lo:
-.byte (205008 >> 0) & $FF
+.byte (205042 >> 0) & $FF
 
 evi_ptr_list_hi:
-.byte ((205008 >> 8) & $1F) + $80
+.byte ((205042 >> 8) & $1F) + $80
 
 evi_ptr_list_bnk:
-.byte ((205008 >> 13) & $7F) + IMG_BNK
+.byte ((205042 >> 13) & $7F) + IMG_BNK
