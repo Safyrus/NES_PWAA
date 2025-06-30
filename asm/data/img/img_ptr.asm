961c73e924b10bb39f593c71d7b02187429fd60c
Safyrus <54881649+Safyrus@users.noreply.github.com>
2025-06-30 19:14:02 +0200

    fix fonts and update nes files

--- asm/data/img/img_ptr.asm
+++ asm/data/img/img_ptr.asm
@@ -1,20 +1,20 @@
 img_ptr_list_lo:
 .byte (0 >> 0) & $FF
-.byte (101687 >> 0) & $FF
+.byte (101612 >> 0) & $FF
 
 img_ptr_list_hi:
 .byte ((0 >> 8) & $1F) + $80
-.byte ((101687 >> 8) & $1F) + $80
+.byte ((101612 >> 8) & $1F) + $80
 
 img_ptr_list_bnk:
 .byte ((0 >> 13) & $7F) + IMG_BNK
-.byte ((101687 >> 13) & $7F) + IMG_BNK
+.byte ((101612 >> 13) & $7F) + IMG_BNK
 
 evi_ptr_list_lo:
-.byte (205042 >> 0) & $FF
+.byte (204967 >> 0) & $FF
 
 evi_ptr_list_hi:
-.byte ((205042 >> 8) & $1F) + $80
+.byte ((204967 >> 8) & $1F) + $80
 
 evi_ptr_list_bnk:
-.byte ((205042 >> 13) & $7F) + IMG_BNK
+.byte ((204967 >> 13) & $7F) + IMG_BNK
