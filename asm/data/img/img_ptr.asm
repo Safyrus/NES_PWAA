eb974d16881f5cc781e8586f865d7725750bb32e
Safyrus <54881649+Safyrus@users.noreply.github.com>
2025-07-11 21:56:31 +0200

    update snif and asm image data

--- asm/data/img/img_ptr.asm
+++ asm/data/img/img_ptr.asm
@@ -1,20 +1,23 @@
 img_ptr_list_lo:
 .byte (0 >> 0) & $FF
-.byte (101612 >> 0) & $FF
+.byte (104275 >> 0) & $FF
+.byte (212932 >> 0) & $FF
 
 img_ptr_list_hi:
 .byte ((0 >> 8) & $1F) + $80
-.byte ((101612 >> 8) & $1F) + $80
+.byte ((104275 >> 8) & $1F) + $80
+.byte ((212932 >> 8) & $1F) + $80
 
 img_ptr_list_bnk:
 .byte ((0 >> 13) & $7F) + IMG_BNK
-.byte ((101612 >> 13) & $7F) + IMG_BNK
+.byte ((104275 >> 13) & $7F) + IMG_BNK
+.byte ((212932 >> 13) & $7F) + IMG_BNK
 
 evi_ptr_list_lo:
-.byte (204967 >> 0) & $FF
+.byte (214193 >> 0) & $FF
 
 evi_ptr_list_hi:
-.byte ((204967 >> 8) & $1F) + $80
+.byte ((214193 >> 8) & $1F) + $80
 
 evi_ptr_list_bnk:
-.byte ((204967 >> 13) & $7F) + IMG_BNK
+.byte ((214193 >> 13) & $7F) + IMG_BNK
