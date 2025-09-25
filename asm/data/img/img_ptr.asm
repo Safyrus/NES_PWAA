763678949d89e3e7884605762ed39212488e34b8
Safyrus <54881649+Safyrus@users.noreply.github.com>
2025-09-25 18:15:52 +0200

    update asm data and nes files

--- asm/data/img/img_ptr.asm
+++ asm/data/img/img_ptr.asm
@@ -1,23 +1,23 @@
 img_ptr_list_lo:
 .byte (0 >> 0) & $FF
-.byte (106403 >> 0) & $FF
-.byte (215173 >> 0) & $FF
+.byte (110713 >> 0) & $FF
+.byte (193284 >> 0) & $FF
 
 img_ptr_list_hi:
 .byte ((0 >> 8) & $1F) + $80
-.byte ((106403 >> 8) & $1F) + $80
-.byte ((215173 >> 8) & $1F) + $80
+.byte ((110713 >> 8) & $1F) + $80
+.byte ((193284 >> 8) & $1F) + $80
 
 img_ptr_list_bnk:
 .byte ((0 >> 13) & $7F) + IMG_BNK
-.byte ((106403 >> 13) & $7F) + IMG_BNK
-.byte ((215173 >> 13) & $7F) + IMG_BNK
+.byte ((110713 >> 13) & $7F) + IMG_BNK
+.byte ((193284 >> 13) & $7F) + IMG_BNK
 
 evi_ptr_list_lo:
-.byte (230325 >> 0) & $FF
+.byte (212725 >> 0) & $FF
 
 evi_ptr_list_hi:
-.byte ((230325 >> 8) & $1F) + $80
+.byte ((212725 >> 8) & $1F) + $80
 
 evi_ptr_list_bnk:
-.byte ((230325 >> 13) & $7F) + IMG_BNK
+.byte ((212725 >> 13) & $7F) + IMG_BNK
--- asm/data/txt_data.asm
+++ asm/data/txt_data.asm
@@ -6,9 +6,24 @@
 .segment "CODE_BNK"
 lz_bnk_table:
 .byte TXT_BNK + $00
+.byte TXT_BNK + $00
+.byte TXT_BNK + $01
+.byte TXT_BNK + $01
+.byte TXT_BNK + $02
+.byte TXT_BNK + $02
 
 lz_adr_table_lo:
 .byte $00
+.byte $E4
+.byte $EC
+.byte $81
+.byte $65
+.byte $EC
 
 lz_adr_table_hi:
 .byte $80
+.byte $92
+.byte $84
+.byte $95
+.byte $86
+.byte $97
